06e73aeeb13c53beb399da06175492310351b1a8
MrAnyx <44176707+MrAnyx@users.noreply.github.com>
2019-10-28 10:38:26 +0100

    [add] main file

---
+++ app.py
@@ -0,0 +1,44 @@
+
+# Librairies
+from flask import Flask, jsonify
+import pymysql
+from dotenv import load_dotenv, find_dotenv
+import os
+
+
+# Variables
+load_dotenv(find_dotenv())
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.env = "dev"
+
+
+
+
+# connection à la base de données
+def connection():
+	return pymysql.connect(host = os.getenv("DB_HOST"),
+		user = os.getenv("DB_USER"),
+		password = os.getenv("DB_PASS"),
+		db = os.getenv("DB_NAME"),
+		charset = "utf8mb4",
+		cursorclass = pymysql.cursors.DictCursor)
+
+
+
+# Routes disponibles
+@app.route("/", methods=["GET"])
+def home():
+	con = connection()
+	with con:
+		cur = con.cursor()
+		sql = 'SELECT * from users'
+		cur.execute(sql,())
+		result = cur.fetchall()
+		return jsonify(result), 200
+
+
+
+# Lancement de l'application
+if __name__ == "__main__":
+	app.run()
